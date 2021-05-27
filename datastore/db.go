54e0e0a535ebc81190ba9771a01ecac64171c097
dimakryshtal <dima.krshl@gmail.com>
2021-05-27 23:26:17 +0300

    task 1 finished(maybe)

--- datastore/db.go
+++ datastore/db.go
@@ -7,32 +7,40 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strconv"
 )
 
-const outFileName = "current-data"
+var segments []Db
+
+const currentFile = "current-data"
+const outFileName = "segment-"
+
+var tempDir string
 
 var ErrNotFound = fmt.Errorf("record does not exist")
 
 type hashIndex map[string]int64
 
 type Db struct {
-	out *os.File
-	outPath string
+	out       *os.File
+	outPath   string
 	outOffset int64
 
 	index hashIndex
 }
 
-func NewDb(dir string) (*Db, error) {
-	outputPath := filepath.Join(dir, outFileName)
+//outFileName+strconv.Itoa(len(segments)+1
+func NewDb(filename, dir string) (*Db, error) {
+	outputPath := filepath.Join(dir, filename)
 	f, err := os.OpenFile(outputPath, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0o600)
 	if err != nil {
 		return nil, err
 	}
 	db := &Db{
-		outPath: outputPath,
-		out:     f,
-		index:   make(hashIndex),
+		outPath:   outputPath,
+		out:       f,
+		outOffset: 0,
+		index:     make(hashIndex),
 	}
 	err = db.recover()
 	if err != nil && err != io.EOF {
@@ -41,7 +49,92 @@ func NewDb(dir string) (*Db, error) {
 	return db, nil
 }
 
-const bufSize = 8192
+func mergeSegments() error {
+	var segmentsMerged []Db
+	mergedDb, err := NewDb(outFileName+strconv.Itoa(len(segments)+1), tempDir)
+	mergedDb.outOffset = 0
+	if err != nil {
+		return err
+	}
+	for _, el := range segments {
+		for key := range el.index {
+			segIndex, position, ok := getLastFromSegments(key)
+			if segIndex != nil && ok {
+				file, err := os.Open(segments[*segIndex].outPath)
+				if err != nil {
+					return err
+				}
+				_, err = file.Seek(position, 0)
+				if err != nil {
+					return err
+				}
+
+				reader := bufio.NewReader(file)
+				value, err := readValue(reader)
+				if err != nil {
+					return err
+				}
+				e := entry{
+					key:   key,
+					value: value,
+				}
+				encoded := e.Encode()
+				if int(mergedDb.outOffset)+len(encoded) > bufSize {
+					mergedDb.Close()
+					segmentsMerged = append(segmentsMerged, *mergedDb)
+					mergedDb, err = NewDb(outFileName+strconv.Itoa(len(segments)+1+len(segmentsMerged)), tempDir)
+					if err != nil {
+						return err
+					}
+
+				}
+				n, err := mergedDb.out.Write(e.Encode())
+				if err == nil {
+					mergedDb.index[key] = mergedDb.outOffset
+					mergedDb.outOffset += int64(n)
+				}
+				file.Close()
+			}
+			for i := 0; i <= *segIndex; i++ {
+				_, ok := segments[i].index[key]
+				if ok {
+					delete(segments[i].index, key)
+				}
+			}
+		}
+		if len(el.index) == 0 {
+			os.Remove(el.outPath)
+		}
+	}
+	mergedDb.Close()
+	segments = append(segmentsMerged, *mergedDb)
+	for i, el := range segments {
+		err := os.Rename(el.outPath, tempDir+`\`+outFileName+strconv.Itoa(i+1))
+		if err != nil {
+			return err
+		}
+		segments[i].outPath = tempDir + `\` + outFileName + strconv.Itoa(i+1)
+	}
+	return nil
+}
+
+func getLastFromSegments(key string) (*int, int64, bool) {
+	var currentSegment *int
+	i := len(segments) - 1
+	for i >= 0 {
+		currentSegment = &i
+		position, ok := segments[i].index[key]
+		if ok {
+			return currentSegment, position, ok
+
+		} else {
+			i--
+		}
+	}
+	return nil, 0, false
+}
+
+const bufSize = 200
 
 func (db *Db) recover() error {
 	input, err := os.Open(db.outPath)
@@ -55,7 +148,7 @@ func (db *Db) recover() error {
 	for err == nil {
 		var (
 			header, data []byte
-			n int
+			n            int
 		)
 		header, err = in.Peek(bufSize)
 		if err == io.EOF {
@@ -93,12 +186,23 @@ func (db *Db) Close() error {
 }
 
 func (db *Db) Get(key string) (string, error) {
+	var currentSegment *int
+	var file *os.File
+	var err error
+
 	position, ok := db.index[key]
 	if !ok {
-		return "", ErrNotFound
+		currentSegment, position, ok = getLastFromSegments(key)
+		if !ok {
+			return "", ErrNotFound
+		}
 	}
 
-	file, err := os.Open(db.outPath)
+	if currentSegment != nil {
+		file, err = os.Open(segments[*currentSegment].outPath)
+	} else {
+		file, err = os.Open(db.outPath)
+	}
 	if err != nil {
 		return "", err
 	}
@@ -114,18 +218,35 @@ func (db *Db) Get(key string) (string, error) {
 	if err != nil {
 		return "", err
 	}
+
 	return value, nil
 }
 
-func (db *Db) Put(key, value string) error {
+func (db *Db) Put(key, value string) (*Db, error) {
 	e := entry{
 		key:   key,
 		value: value,
 	}
+	encoded := e.Encode()
+	if int(db.outOffset)+len(encoded) > bufSize {
+		db.Close()
+		err := os.Rename(db.outPath, tempDir+`\`+outFileName+strconv.Itoa(len(segments)+1))
+		if err != nil {
+			return nil, err
+		}
+		db.outPath = tempDir + `\` + outFileName + strconv.Itoa(len(segments)+1)
+		segments = append(segments, *db)
+		db, err = NewDb(currentFile, tempDir)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	n, err := db.out.Write(e.Encode())
 	if err == nil {
 		db.index[key] = db.outOffset
 		db.outOffset += int64(n)
+		return db, nil
 	}
-	return err
+	return nil, err
 }
--- datastore/db_test.go
+++ datastore/db_test.go
@@ -9,33 +9,42 @@ import (
 
 func TestDb_Put(t *testing.T) {
 	dir, err := ioutil.TempDir("", "test-db")
+	tempDir = dir
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer os.RemoveAll(dir)
 
-	db, err := NewDb(dir)
+	db, err := NewDb(currentFile, dir)
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	defer db.Close()
 
-	pairs := [][]string {
+	pairs := [][]string{
 		{"key1", "value1"},
 		{"key2", "value2"},
 		{"key3", "value3"},
 	}
 
-	outFile, err := os.Open(filepath.Join(dir, outFileName))
+	outFile, err := os.Open(filepath.Join(dir, currentFile))
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	t.Run("put/get", func(t *testing.T) {
 		for _, pair := range pairs {
-			err := db.Put(pair[0], pair[1])
+			newDb, err := db.Put(pair[0], pair[1])
 			if err != nil {
 				t.Errorf("Cannot put %s: %s", pairs[0], err)
+			}
+			if newDb != nil {
+				outFile, err = os.Open(filepath.Join(dir, currentFile))
+				if err != nil {
+					t.Errorf("Cannot put %s: %s", pairs[0], err)
+				}
+				db = newDb
 			}
 			value, err := db.Get(pair[0])
 			if err != nil {
@@ -45,6 +54,7 @@ func TestDb_Put(t *testing.T) {
 				t.Errorf("Bad value returned expected %s, got %s", pair[1], value)
 			}
 		}
+
 	})
 
 	outInfo, err := outFile.Stat()
@@ -55,16 +65,23 @@ func TestDb_Put(t *testing.T) {
 
 	t.Run("file growth", func(t *testing.T) {
 		for _, pair := range pairs {
-			err := db.Put(pair[0], pair[1])
+			newDb, err := db.Put(pair[0], pair[1])
 			if err != nil {
 				t.Errorf("Cannot put %s: %s", pairs[0], err)
+			}
+			if newDb != nil {
+				outFile, err = os.Open(filepath.Join(dir, currentFile))
+				if err != nil {
+					t.Errorf("Cannot put %s: %s", pairs[0], err)
+				}
+				db = newDb
 			}
 		}
 		outInfo, err := outFile.Stat()
 		if err != nil {
 			t.Fatal(err)
 		}
-		if size1 * 2 != outInfo.Size() {
+		if size1*2 != outInfo.Size() {
 			t.Errorf("Unexpected size (%d vs %d)", size1, outInfo.Size())
 		}
 	})
@@ -73,7 +90,7 @@ func TestDb_Put(t *testing.T) {
 		if err := db.Close(); err != nil {
 			t.Fatal(err)
 		}
-		db, err = NewDb(dir)
+		db, err = NewDb(currentFile, dir)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -88,5 +105,87 @@ func TestDb_Put(t *testing.T) {
 			}
 		}
 	})
+}
 
+func TestDB_Segmentation(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test-db")
+	tempDir = dir
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	db, err := NewDb(currentFile, dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer db.Close()
+
+	pairs := [][]string{
+		{"key1", "value1"},
+		{"key2", "value2"},
+		{"key3", "value3"},
+		{"key4", "value3"},
+		{"key5", "value3"},
+		{"key6", "value3"},
+		{"key7", "value3"},
+		{"key8", "value3"},
+		{"key9", "value3"},
+		{"key10", "value3"},
+		{"key11", "value3"},
+		{"key12", "value3"},
+		{"key13", "value3"},
+		{"key14", "value3"},
+		{"key15", "value3"},
+		{"key16", "value3"},
+		{"key17", "value3"},
+	}
+
+	t.Run("test segmentation", func(t *testing.T) {
+		for i := 0; i < 3; i++ {
+			for _, pair := range pairs {
+				newDb, err := db.Put(pair[0], pair[1])
+				if err != nil {
+					t.Errorf("Cannot put %s: %s", pairs[0], err)
+				}
+				if newDb != nil {
+					db = newDb
+				}
+			}
+		}
+		numOfSegs := len(segments)
+		if numOfSegs != 5 {
+			t.Errorf("Wrong number of segments: %d", numOfSegs)
+		}
+
+		value, err := db.Get("key2")
+		if err != nil {
+			t.Errorf("Cannot get %s: %s", "key2", err)
+		}
+		if value != "value2" {
+			t.Errorf("Bad value returned expected %s, got %s", "value2", value)
+		}
+
+	})
+
+	t.Run("check merge", func(t *testing.T) {
+
+		err := mergeSegments()
+		if err != nil {
+			t.Errorf("Error while merging: %s", err)
+		}
+		if len(segments) != 2 {
+			t.Errorf("Wrong number of segments: %d, expected %d", len(segments), 1)
+		}
+
+		value, err := db.Get("key3")
+		if err != nil {
+			t.Errorf("Cannot get %s: %s", pairs[0], err)
+		}
+		if value != "value3" {
+			t.Errorf("Bad value returned expected %s, got %s", "value3", value)
+		}
+
+	})
 }
